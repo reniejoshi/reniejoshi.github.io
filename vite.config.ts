616daec7b1d4ddd5707b69305cff6474e8e789fd
Renie Joshi <177680549+reniejoshi@users.noreply.github.com>
2026-07-24 15:56:38 -0700

    ci: set up deploy workflow and configure vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,69 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  base: '/',
+  plugins: [
+    react(),
+    tailwindcss(),
+    ViteImageOptimizer({
+      logStats: true,
+      ansiColors: true,
+      test: /\.(jpe?g|png|gif|webp|svg)$/i,
+      exclude: undefined,
+      include: undefined,
+      includePublic: false,
+      svg: {
+        multipass: true,
+        plugins: [
+          {
+            name: 'preset-default',
+            params: {
+              overrides: {
+                cleanupNumericValues: false,
+                cleanupIds: {
+                  minify: false,
+                  remove: false,
+                },
+                convertPathData: false,
+              },
+            },
+          },
+          'sortAttrs',
+          {
+            name: 'addAttributesToSVGElement',
+            params: {
+              attributes: [{ xmlns: 'http://www.w3.org/2000/svg' }],
+            },
+          },
+        ],
+      },
+      png: {
+        quality: 85,
+      },
+      jpeg: {
+        quality: 85,
+      },
+      jpg: {
+        quality: 85,
+      },
+      gif: {},
+      webp: {
+        lossless: true,
+      },
+      cache: false,
+      cacheLocation: undefined,
+    }),
+  ],
+
+  server: {
+    port: 9500,
+    host: true,
+  },
+  preview: {
+    port: 9500,
+    host: true,
+  },
 })
